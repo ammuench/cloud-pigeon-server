4facad0ab64ba0ae650d2afb2fee394ccc22fc90
Alex Muench <ammuench@gmail.com>
2023-04-23 12:39:58 -0500

    ✨ Move out of monorepo, setup dev environment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  //@ts-ignore
+  Component,
+  //@ts-ignore
+  pageProps,
+}) {
+  return <Component {...pageProps} />;
 }
